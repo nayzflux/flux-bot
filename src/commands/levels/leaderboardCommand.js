d49ad3287f6e228be16e45bd1bf8a690484f6774
NayZBySodium <nayzbysodium@users.noreply.github.com>
2022-09-18 19:23:28 +0200

    /leaderboard patch

--- src/commands/levels/leaderboardCommand.js
+++ src/commands/levels/leaderboardCommand.js
@@ -29,35 +29,39 @@ module.exports = {
         let str = ``;
 
         for (level of leaderboards) {
-            const member = await interaction.guild.members.fetch(level.userId);
+            try {
+                const member = await interaction.guild.members.fetch(level.userId);
 
-            if (member) {
-                i++;
+                if (member) {
+                    i++;
 
-                if (i === 1) {
-                    str = str + `🥇 - ${member} ${level.level} (${level.xp} XP)\n`
+                    if (i === 1) {
+                        str = str + `🥇 - ${member} ${level.level} (${level.xp} XP)\n`
+                    }
+
+                    if (i === 2) {
+                        str = str + `🥈 - ${member} ${level.level} (${level.xp} XP)\n`
+                    }
+
+                    if (i === 3) {
+                        str = str + `🥉 - ${member} ${level.level} (${level.xp} XP)\n`
+                    }
+
+
+                    if (i === 4) {
+                        str = str + `4️⃣ - ${member} ${level.level} (${level.xp} XP)\n`
+                    }
+
+                    if (i === 5) {
+                        str = str + `5️⃣ - ${member} ${level.level} (${level.xp} XP)\n`
+                    }
+
+                    if (i !== 1 && i !== 2 && i !== 3 && i !== 4 && i !== 5) {
+                        str = str + `${i} - ${member} ${level.level} (${level.xp} XP)\n`
+                    }
                 }
-
-                if (i === 2) {
-                    str = str + `🥈 - ${member} ${level.level} (${level.xp} XP)\n`
-                }
-
-                if (i === 3) {
-                    str = str + `🥉 - ${member} ${level.level} (${level.xp} XP)\n`
-                }
-
-
-                if (i === 4) {
-                    str = str + `4️⃣ - ${member} ${level.level} (${level.xp} XP)\n`
-                }
-
-                if (i === 5) {
-                    str = str + `5️⃣ - ${member} ${level.level} (${level.xp} XP)\n`
-                }
-
-                if (i !== 1 && i !== 2 && i !== 3 && i !== 4 && i !== 5) {
-                    str = str + `${i} - ${member} ${level.level} (${level.xp} XP)\n`
-                }
+            } catch (err) {
+                console.log(`❌ Membre introuvable`);
             }
         }
 
